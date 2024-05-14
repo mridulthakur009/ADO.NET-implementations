b295e5d69fbae6217828381507fc959e873eadfc
mridulthakur009 <mridul.thakur.007@gmail.com>
2024-05-14 11:47:44 +0530

    Load XML data into SQL Server.

---
+++ ADO.NET implementations/LoadBulkData.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ADO.NET_implementations
+{
+    public partial class LoadBulkData : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection())
+            {
+                DataSet ds = new DataSet();
+                ds.ReadXml(Server.MapPath("~/Data.xml"));
+
+                DataTable dtDept = ds.Tables["Department"];
+                DataTable dtEmp = ds.Tables["Employee"];
+
+                con.Open();
+
+                using (SqlBulkCopy sbc = new SqlBulkCopy(con))
+                {
+                    sbc.DestinationTableName = "Departments";
+                    sbc.ColumnMappings.Add("ID", "ID");
+                    sbc.ColumnMappings.Add("Name", "Name");
+                    sbc.ColumnMappings.Add("Location", "Location");
+
+                    sbc.WriteToServer(dtDept);
+                }
+                using (SqlBulkCopy sbc = new SqlBulkCopy(con))
+                {
+                    sbc.DestinationTableName = "Employees";
+                    sbc.ColumnMappings.Add("ID", "ID");
+                    sbc.ColumnMappings.Add("Name", "Name");
+                    sbc.ColumnMappings.Add("Gender", "Gender");
+                    sbc.ColumnMappings.Add("DepartmentId", "DepartmentId");
+
+                    sbc.WriteToServer(dtEmp);
+                }
+            }
+        }
+    }
+}
