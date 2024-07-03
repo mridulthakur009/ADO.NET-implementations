e201cac3203559323be6fc02b995397c71e34f63
mridulthakur009 <mridul.thakur.007@gmail.com>
2024-07-03 09:55:09 +0530

    did some improvement in code of excel

--- ADO.NET implementations/WorkingWithExcel.aspx.designer.cs
+++ ADO.NET implementations/WorkingWithExcel.aspx.designer.cs
@@ -40,5 +40,14 @@ namespace ADO.NET_implementations
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnRead;
+
+        /// <summary>
+        /// GridView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
     }
 }
